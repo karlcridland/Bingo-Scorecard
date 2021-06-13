2f9b4ffc644180d4489acc4454ea5bd36bcd0307
Karl Cridland <karlcridland@gmail.com>
2021-06-13 09:43:41 +0100

    Created preliminary classes for the game.

---
+++ Bingo Scorecard/Board.swift
@@ -0,0 +1,8 @@
+//
+//  Board.swift
+//  Bingo Scorecard
+//
+//  Created by Karl Cridland on 13/06/2021.
+//
+
+import Foundation
---
+++ Bingo Scorecard/Tile.swift
@@ -0,0 +1,8 @@
+//
+//  Tile.swift
+//  Bingo Scorecard
+//
+//  Created by Karl Cridland on 13/06/2021.
+//
+
+import Foundation
